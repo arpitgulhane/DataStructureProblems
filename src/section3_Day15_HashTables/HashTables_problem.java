5e2acc7195b5da5fd6109d8f80ac3596945d89c0
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-13 13:07:42 +0530

    frequency of words in large paragraph

--- src/section3_Day15_HashTables/HashTables_problem.java
+++ src/section3_Day15_HashTables/HashTables_problem.java
@@ -1,27 +1,55 @@
 package section3_Day15_HashTables;
 
 import java.util.Hashtable;
+import java.util.LinkedList;
 
 public class HashTables_problem {
     public static void main(String[] args) {
+        // uc1
+//        String sentence = "To be or not to be";
+//        Hashtable<String, Integer> freq = new Hashtable<>();
+//
+//        // Split the sentence into words
+//        String[] words = sentence.split(" ");
+//
+//        // Count the frequency of each word
+//        for (String word : words) {
+//            if (freq.containsKey(word)) {
+//                freq.put(word, freq.get(word) + 1);
+//            } else {
+//                freq.put(word, 1);
+//            }
+//        }
+//
+//        // Print the frequency of each word
+//        for (String word : freq.keySet()) {
+//            System.out.println(word + ": " + freq.get(word));
+//        }
+
+// uc2
         String sentence = "To be or not to be";
-        Hashtable<String, Integer> freq = new Hashtable<>();
+        Hashtable<String, LinkedList<Integer>> wordFreq = new Hashtable<>();
 
-        // Split the sentence into words
-        String[] words = sentence.split(" ");
+        // Convert sentence to lowercase and split into array of words
+        String[] words = sentence.toLowerCase().split("\\W+");
 
-        // Count the frequency of each word
+        // Iterate over each word and update the frequency in the Hashtable
         for (String word : words) {
-            if (freq.containsKey(word)) {
-                freq.put(word, freq.get(word) + 1);
+            LinkedList<Integer> freqList = wordFreq.get(word);
+            if (freqList == null) {
+                freqList = new LinkedList<>();
+                freqList.add(1);
+                wordFreq.put(word, freqList);
             } else {
-                freq.put(word, 1);
+                freqList.add(freqList.getLast() + 1);
             }
         }
 
-        // Print the frequency of each word
-        for (String word : freq.keySet()) {
-            System.out.println(word + ": " + freq.get(word));
+        // Print out the frequency of each word in the sentence
+        for (String word : wordFreq.keySet()) {
+            LinkedList<Integer> freqList = wordFreq.get(word);
+            int frequency = freqList.getLast();
+            System.out.println(word + ": " + frequency);
         }
 
     }
